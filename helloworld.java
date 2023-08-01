9d6b631de0a64d59c1556d60f88d39a6db60f655
ct-RaviTeja66 <raviteja.g@cartrade.com>
2023-08-01 18:06:08 +0530

    test2

--- helloworld.java
+++ helloworld.java
@@ -2,4 +2,7 @@ class test{
         public static void main(String[] args){
                 System.out.println("Hello World");
         }
+        public static void test(String[] args){
+                System.out.println("Hello World");
+        }
 }
