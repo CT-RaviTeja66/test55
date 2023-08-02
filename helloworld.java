8e9df7bc9b32542e9abdb89bba632f608792c7ca
CT-RaviTeja66 <134708371+CT-RaviTeja66@users.noreply.github.com>
2023-08-02 10:51:10 +0530

    Update helloworld.java
    new plugins

--- helloworld.java
+++ helloworld.java
@@ -1,9 +1,14 @@
 class test{
         public static void main(String[] args){
                 System.out.println("Hello World");
-                test();
+                test1();
+                test2();
         }
         public static void test(){
-                System.out.println("Hello World");
+                System.out.println("Hello test1");
+        }     
+}
+public static void test2(){
+                System.out.println("Hello test2");
         }     
 }
